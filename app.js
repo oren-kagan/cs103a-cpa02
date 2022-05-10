201cf87a6518424d0b3ccfb82b03879a6e3e8e8f
oren-kagan <99365184+oren-kagan@users.noreply.github.com>
2022-05-10 16:53:42 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+// *********************************************************** //
+//  Loading packages to support the server
+// *********************************************************** //
+// First we load in all of the packages we need for the server...
+const createError = require("http-errors"); // to handle the server errors
+const express = require("express");
+const path = require("path");  // to refer to local paths
+const cookieParser = require("cookie-parser"); // to handle cookies
+const session = require("express-session"); // to handle sessions using cookies
+const debug = require("debug")("personalapp:server"); 
+const layouts = require("express-ejs-layouts");
+const axios = require("axios")
+
+// *********************************************************** //
+//  Connecting to the database
+// *********************************************************** //
+
+const mongoose = require( 'mongoose' ); 
+const mongoDB_URI = 'mongodb+srv://blueoren:SvhRsS6e5ICtT8GA@firstcluster.ebsr5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongoose.connect( mongodb_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true } );
+// fix deprecation warnings....
+mongoose.set('useFindAndModify', false); 
+mongoose.set('useCreateIndex', true);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {console.log("we are connected!!!")});
+
+// *********************************************************** //
+// Initializing the Express server 
+//
+// This code is run once when the app is started and it creates
+// a server that respond to requests by sending responses
+// *********************************************************** //
+const app = express();
+
+// Here we specify that we will be using EJS as our view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// this allows us to use page layout for the views 
+// so we don't have to repeat the headers and footers on every page ...
+// the layout is in views/layout.ejs
+app.use(layouts);
+
+// Here we process the requests so they are easy to handle
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Here we specify that static files will be in the public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Here we enable session handling using cookies
+app.use(
+  session({
+    secret: "zzbbyanana789sdfa8f9ds8f90ds87f8d9s789fds", // this ought to be hidden in process.env.SECRET
+    resave: false,
+    saveUninitialized: false
+  })
+);
